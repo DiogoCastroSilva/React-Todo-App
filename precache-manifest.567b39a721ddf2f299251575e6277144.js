4bb4ef53848a02cb0805fca3a84a4e81ac5f5088
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2019-07-07 19:10:06 +0100

    Updates

---
+++ precache-manifest.567b39a721ddf2f299251575e6277144.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d490de40f143987c2f7ce6f59c0fa6be",
+    "url": "/React-Todo-App/index.html"
+  },
+  {
+    "revision": "9fa8b8e745d32f98eb54",
+    "url": "/React-Todo-App/static/css/main.dd18ab3c.chunk.css"
+  },
+  {
+    "revision": "b9f430225d9171462482",
+    "url": "/React-Todo-App/static/js/2.02169f29.chunk.js"
+  },
+  {
+    "revision": "9fa8b8e745d32f98eb54",
+    "url": "/React-Todo-App/static/js/main.95e2c1a5.chunk.js"
+  },
+  {
+    "revision": "61b135619dd31ef8e702",
+    "url": "/React-Todo-App/static/js/runtime~main.c2279f55.js"
+  }
+]);
